61bb459a7df8e18a99e0f730b64a4b8882954dce
cryptoroshan <royalguy1031@gmail.com>
2024-01-13 20:52:50 +0000

    x

--- src/main/java/com/serenitydojo/wordle/microservices/authentication/ProtectedResourceController.java
+++
@@ -1,9 +0,0 @@
-@RestController
-public class ProtectedResourceController {
-
-    @GetMapping("/protected")
-    @Secured("ROLE_USER") // Role-based authorization
-    public String protectedResource() {
-        return "This is a protected resource.";
-    }
-}
