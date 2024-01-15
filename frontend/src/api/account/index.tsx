79453c0bdac4410eaca8d137b27c568544a0ddd2
cryptoroshan <royalguy1031@gmail.com>
2024-01-15 12:29:02 +0000

    added signin and signup pages

---
+++ frontend/src/api/account/index.tsx
@@ -0,0 +1,61 @@
+import { SERVER_API_URL } from "../config";
+import axios from "axios";
+import { jwtDecode } from 'jwt-decode';
+
+axios.defaults.headers.common['Content-Type'] = 'application/json';
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+const STORAGE_KEY = "token";
+
+axios.interceptors.response.use(function (response) {
+  return response;
+}, function (error) {
+  console.log('error >>> ', error);
+  if (error.response.status === 401 && error.response.data.status === 'fail' && error.response.data.message.name === 'TokenExpiredError') {
+    window.location.href = '/auth';
+  } else {
+    return Promise.reject(error);
+  }
+});
+
+axios.interceptors.request.use(function (config) {
+  const accessToken = sessionStorage.getItem(STORAGE_KEY);
+  if (accessToken != null) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+  return config;
+}, function (error) {
+  return Promise.reject(error);
+});
+
+class AccountApi {
+  me(request: any) {
+    const { accessToken } = request
+
+    return new Promise((resolve, reject) => {
+      try {
+        // Decode access token
+        const decodedToken: any = jwtDecode(accessToken)
+        console.log(' >>> decodeToken >>> ', decodedToken);
+
+        const currrentTime = Date.now() / 1000;
+
+        if (decodedToken.expires < currrentTime) {
+          // Token has expired
+          reject(new Error('Token expired'));
+        } else {
+          const { _id, name, email } = decodedToken;
+          resolve({
+            _id: _id,
+            name: name,
+            email: email
+          });
+        }
+      } catch (err) {
+        console.error('[Account Api]: ', err)
+        reject(new Error('Internal server error'))
+      }
+    })
+  }
+}
+
+export const accountApi = new AccountApi()
---
+++ frontend/src/provider/authProvider.tsx
@@ -0,0 +1,200 @@
+// import { createContext, useContext, useEffect, useMemo, useState } from "react";
+import { createContext, useCallback, useEffect, useReducer, useContext } from "react";
+
+import { accountApi } from "../api/account";
+
+const STORAGE_KEY = "token";
+
+let ActionType : any;
+(function (ActionType) {
+  ActionType["INITIALIZE"] = "INITIALIZE";
+  ActionType["SIGN_IN"] = "SIGN_IN";
+  ActionType["SIGN_UP"] = "SIGN_UP";
+  ActionType["SIGN_OUT"] = "SIGN_OUT";
+})(ActionType || (ActionType = {}));
+
+const initialState = {
+  isAuthenticated: false,
+  isInitialized: false,
+  account: null,
+  accessToken: null,
+};
+
+const handlers: any = {
+  INITIALIZE: (state: any, action: any) => {
+    const { isAuthenticated, account, accessToken } = action.payload;
+
+    return {
+      ...state,
+      isAuthenticated,
+      isInitialized: true,
+      account,
+      accessToken
+    };
+  },
+  SIGN_IN: (state: any, action: any) => {
+    const { account, accessToken } = action.payload;
+
+    return {
+      ...state,
+      isAuthenticated: true,
+      account,
+      accessToken
+    };
+  },
+  SIGN_UP: (state: any, action: any) => {
+    const { account } = action.payload;
+
+    return {
+      ...state,
+      isAuthenticated: true,
+      account,
+    };
+  },
+  SIGN_OUT: (state: any) => ({
+    ...state,
+    isAuthenticated: false,
+    account: null,
+    accessToken : null
+  })
+};
+
+const reducer = (state: any, action: any) =>
+  handlers[action.type] ? handlers[action.type](state, action) : state;
+
+export const AuthContext = createContext({
+  ...initialState,
+  signIn: () => Promise.resolve(),
+  signOut: () => Promise.resolve(),
+});
+
+export const AuthProvider = (props: any) => {
+  const { children } = props;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const initialize = useCallback(async () => {
+    try {
+      const accessToken = window.sessionStorage.getItem(STORAGE_KEY);
+
+      if (accessToken) {
+        const account = await accountApi.me({ accessToken });
+
+        dispatch({
+          type: ActionType.INITIALIZE,
+          payload: {
+            isAuthenticated: true,
+            account,
+            accessToken
+          },
+        });
+        
+      } else {
+        dispatch({
+          type: ActionType.INITIALIZE,
+          payload: {
+            isAuthenticated: false,
+            account: null,
+            accessToken : null
+          },
+        });
+      }
+    } catch (err) {
+      console.error(err);
+      dispatch({
+        type: ActionType.INITIALIZE,
+        payload: {
+          isAuthenticated: false,
+          account: null,
+          accessToken: null
+        },
+      });
+    }
+  }, [dispatch]);
+
+  useEffect(
+    () => {
+      initialize();
+    },
+    []
+  );
+
+  const signIn = useCallback(
+    async (accessToken) => {
+      const account = await accountApi.me({ accessToken });
+
+      sessionStorage.setItem(STORAGE_KEY, accessToken);
+
+      dispatch({
+        type: ActionType.SIGN_IN,
+        payload: {
+          account,
+          accessToken
+        },
+      });
+    },
+    [dispatch]
+  );
+
+  const signOut = useCallback(async () => {
+    sessionStorage.removeItem(STORAGE_KEY);
+    dispatch({ type: ActionType.SIGN_OUT });
+  }, [dispatch]);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        ...state,
+        signIn,
+        signOut,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+
+
+
+
+// const AuthContext = createContext();
+
+// const AuthProvider = ({ children }) => {
+//   // State to hold the authentication token
+//   const [token, setToken_] = useState(localStorage.getItem("token"));
+
+//   // Function to set the authentication token
+//   const setToken = (newToken) => {
+//     setToken_(newToken);
+//   };
+
+//   useEffect(() => {
+//     if (token) {
+//       // axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+//       localStorage.setItem('token', token);
+//     } else {
+//       // delete axios.defaults.headers.common["Authorization"];
+//       localStorage.removeItem('token')
+//     }
+//   }, [token]);
+
+//   // Memoized value of the authentication context
+//   const contextValue = useMemo(
+//     () => ({
+//       token,
+//       setToken,
+//     }),
+//     [token]
+//   );
+
+//   // Provide the authentication context to the children components
+//   return (
+//     <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+//   );
+// };
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export default AuthProvider;
