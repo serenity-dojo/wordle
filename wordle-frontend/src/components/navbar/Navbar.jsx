529e5d09f80bd3462305a6cd6552dc96042d3764
cryptoroshan <royalguy1031@gmail.com>
2024-01-18 19:31:06 +0000

    auto start new game & logout

--- wordle-frontend/src/components/navbar/Navbar.jsx
+++ wordle-frontend/src/components/navbar/Navbar.jsx
@@ -3,36 +3,41 @@ import {
   ChartBarIcon,
   CogIcon,
   InformationCircleIcon,
-} from '@heroicons/react/outline'
+} from "@heroicons/react/outline";
 import { toast } from "react-toastify";
 
-import { ENABLE_ARCHIVED_GAMES } from '../../constants/settings'
-import { GAME_TITLE } from '../../constants/strings'
-import { start_new_game, find_hint } from '../../api/api'
+import { ENABLE_ARCHIVED_GAMES } from "../../constants/settings";
+import { GAME_TITLE } from "../../constants/strings";
+import { start_new_game, find_hint } from "../../api/api";
+import { useAuth } from "../../provider/authProvider";
 
 export const Navbar = ({
   setIsInfoModalOpen,
   setIsStatsModalOpen,
   setIsDatePickerModalOpen,
   setIsSettingsModalOpen,
-  handleNewGame
+  handleNewGame,
 }) => {
+  const { signOut } = useAuth();
+
   const newStartGame = async () => {
     const result = await start_new_game();
-    if (result === true)
-      handleNewGame();
-    else
-      toast.error("error");
-  }
+    if (result === true) handleNewGame();
+    else toast.error("error");
+  };
 
   const findHint = async () => {
     find_hint();
+  };
+
+  const logOut = async () => {
+    await signOut();
   }
 
   return (
     <div className="navbar">
       <div className="flex h-[3rem] items-center justify-between px-5 short:h-auto">
-        <div className="flex w-1/3">
+        <div className="flex items-center gap-2 w-1/3">
           <InformationCircleIcon
             className="h-6 w-6 cursor-pointer dark:stroke-white"
             onClick={() => setIsInfoModalOpen(true)}
@@ -43,19 +48,28 @@ export const Navbar = ({
               onClick={() => setIsDatePickerModalOpen(true)}
             />
           )}
+          <button
+            type="button"
+            className="inline-flex w-auto items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-center text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm"
+            onClick={logOut}
+          >
+            Logout
+          </button>
         </div>
-        <p className="text-xl text-center font-bold dark:text-white w-1/3">{GAME_TITLE}</p>
+        <p className="text-xl text-center font-bold dark:text-white w-1/3">
+          {GAME_TITLE}
+        </p>
         <div className="right-icons items-center justify-end w-1/3">
-          <div className='flex items-center mr-4'>
+          <div className="flex items-center mr-4">
             <button
               type="button"
               className="inline-flex w-auto items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-center text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm"
               onClick={newStartGame}
             >
-              Start a new game
+              New Game
             </button>
           </div>
-          <div className='flex items-center mr-4'>
+          <div className="flex items-center mr-4">
             <button
               type="button"
               className="inline-flex w-auto items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-center text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm"
@@ -76,5 +90,5 @@ export const Navbar = ({
       </div>
       <hr></hr>
     </div>
-  )
-}
+  );
+};
